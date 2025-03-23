cf331c4cd7b45efeb426776b51b2b69e0e92a5e8
Lucia Heredia <68028960+LuciaHeredia@users.noreply.github.com>
2025-03-23 04:10:13 +0200

    sgs done

--- scripts/create_sg.sh
+++ scripts/create_sg.sh
@@ -3,11 +3,11 @@ source config/config.conf # Private Variables file
 source "$TEMP_VARS" # Temporary Variables file (IDs)
 
 echo "Creating a Security Group for Public Instance..."
-# allows SSH from anywhere
 PUBLIC_SG_ID=$(aws ec2 create-security-group \
     --group-name "PublicInstanceSG" --description "SG for public EC2 instance" \
     --vpc-id $NEW_VPC_ID --region $REGION \
     --query 'GroupId' --output text)
+# allows SSH from anywhere
 aws ec2 authorize-security-group-ingress \
     --group-id $PUBLIC_SG_ID --region $REGION \
     --protocol tcp --port 22 --cidr 0.0.0.0/0
@@ -15,11 +15,11 @@ echo "PUBLIC_SG_ID=$PUBLIC_SG_ID" >> $TEMP_VARS
 echo "--> Created Security Group for Public Instance: $PUBLIC_SG_ID"
 
 echo "Creating a Security Group for Private Instance..."
-# allows SSH only from public instance
 PRIVATE_SG_ID=$(aws ec2 create-security-group \
     --group-name "PrivateInstanceSG" --description "SG for private EC2 instance" \
     --vpc-id $NEW_VPC_ID --region $REGION \
     --query 'GroupId' --output text)
+# allows SSH only from public instance
 aws ec2 authorize-security-group-ingress \
     --group-id $PRIVATE_SG_ID --region $REGION \
     --protocol tcp --port 22 --source-group $PUBLIC_SG_ID
