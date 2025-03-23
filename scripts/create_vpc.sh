c3a270cf6911ad31cf90927e22a378e107ce6dbe
Lucia Heredia <68028960+LuciaHeredia@users.noreply.github.com>
2025-03-23 04:11:43 +0200

    vpc done

--- scripts/create_vpc.sh
+++ scripts/create_vpc.sh
@@ -13,10 +13,10 @@ if [ -z "$NEW_VPC_ID" ]; then
     exit 1
 fi
 
-echo "NEW_VPC_ID=$NEW_VPC_ID" > $TEMP_VARS
-echo "--> Created VPC: $NEW_VPC_ID"
-
 # Enable DNS hostname support (for public subnet)
 aws ec2 modify-vpc-attribute \
     --vpc-id $NEW_VPC_ID --region $REGION \
     --enable-dns-hostnames "{\"Value\":true}"
+
+echo "NEW_VPC_ID=$NEW_VPC_ID" > $TEMP_VARS
+echo "--> Created VPC: $NEW_VPC_ID"
